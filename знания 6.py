387a006f8ff167c490e1015f1ebfc46aa8217c5e
Arseniy2059 <semyashulkovy64@mail.ru>
2024-11-06 19:35:25 +0300

    Update знания 6.py

--- знания 6.py
+++ знания 6.py
@@ -1,20 +1,26 @@
 def personal_sum(numbers):
-    numbers = [1, 2, 3, 4, 5]
-    result = sum(numbers)
+    result = 0
+    incorrect_data = 0
 
-    for num in numbers:
-        sum += num
+    for numbers in numbers:
+        try:
+            result += numbers
+        except TypeError:
+            print(f"Некорректный тип данных для подсчёта суммы - {numbers}")
+            incorrect_data += 1
+        return result, incorrect_data
 
 
 def calculate_average(numbers):
+    try:
+        result, incorrect_data = personal_sum(numbers)
+        return result / (len(numbers) - incorrect_data)
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print(f"В numbers записан некорректный тип данных")
 
-
-
-
-
-
-
-print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
-print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
